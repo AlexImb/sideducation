ec68aa0c92ebf5178e7feb57bc83229fbca9f5a9
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 18:11:37 +0100

    Add demo courses rules

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -8,28 +8,21 @@
         <div
           v-for="course in courses"
           :key="course.id"
-          class="w-full p-5 my-2 mx-6 rounded-md cursor-pointer md:w-1/6 bg-white border shadow hover:shadow-md"
+          class="w-full font-semibold p-5 my-2 mx-6 rounded-md cursor-pointer md:w-1/6 bg-white border shadow hover:shadow-md"
           @click="$router.push({ name: 'course', params: { id: course.id } })"
         >
           {{ course.title }}
         </div>
       </template>
       <div v-else class="w-full p-6">
-        No courses available. Start by adding one!
-      </div>
-
-      <div
-        class="w-full p-5 my-2 mx-6 shadow-md rounded-md cursor-pointer md:w-1/6 border border-indigo-700 bg-white font-semibold"
-        @click="addCourse()"
-      >
-        Add new course
+        No courses available
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { firebaseAuth, firestore, serverTimestamp } from '@/services/firebase';
+import { firestore } from '@/services/firebase';
 
 export default {
   components: {},
@@ -43,24 +36,10 @@ export default {
   },
   methods: {
     fetchCourses() {
-      firestore
-        .collection('course')
-        .where('userId', '==', firebaseAuth.currentUser.uid)
-        .onSnapshot(courses => {
-          this.courses = [];
-          courses.forEach(course => this.courses.push({ id: course.id, ...course.data() }));
-        });
-    },
-    addCourse() {
-      firestore
-        .collection('courses')
-        .add({
-          title: 'New course ' + new Date().toLocaleString(),
-          createdAt: serverTimestamp(),
-          userReference: firestore.doc(`users/${firebaseAuth.currentUser.uid}`),
-          userId: firebaseAuth.currentUser.uid,
-        })
-        .then(course => this.$router.replace({ name: 'course', params: { id: course.id } }));
+      firestore.collection('courses').onSnapshot(courses => {
+        this.courses = [];
+        courses.forEach(course => this.courses.push({ id: course.id, ...course.data() }));
+      });
     },
   },
 };
