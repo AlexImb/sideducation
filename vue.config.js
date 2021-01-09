e60f1bc474572b8b3742ce676ca448636b483101
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 15:57:02 +0100

    Update PWA icon paths

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,12 @@ module.exports = {
     msTileColor: '#6366F1',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon16: 'img/icons/favicon-16x16.png',
+      favicon32: 'img/icons/favicon-32x32.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png',
+    },
   },
 };
