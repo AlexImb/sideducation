b31c5473a8cb8e6f7334797744f25ddaab744af9
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 02:06:11 +0100

    Combine the Firebase config in one variable

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,19 +3,11 @@ import '@firebase/auth';
 import '@firebase/firestore';
 import '@firebase/analytics';
 
-const config = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
-  authDomain: process.env.FIREBASE_CLIENT_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_CLIENT_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_CLIENT_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+console.log(process.env.VUE_APP_FIREBASE_CONFIG);
+
+const config = JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG);
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
-
-firebase.analytics();
 
 export const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firebaseAuth = firebase.auth();
