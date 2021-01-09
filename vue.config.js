eb1673b942ebf673fef39cf23a3b4fb39d968c1a
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 15:53:22 +0100

    Add theme color

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Sideducation',
+    themeColor: '#6366F1',
+    msTileColor: '#6366F1',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+};
