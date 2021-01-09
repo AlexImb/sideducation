4669122fc9313c57ff41ec44150e671236d03173
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 01:32:16 +0100

    Add Firebase service and workflows

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,41 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import { firebaseAuth, firestore } from './services/firebase';
+export default {
+  components: {},
+  data() {
+    return {
+      user: null,
+    };
+  },
+  computed: {},
+  created() {
+    firebaseAuth.onAuthStateChanged(user => {
+      this.user = user;
+
+      if (user) {
+        const userDTO = {
+          ...user.providerData[0],
+          emailVerified: user.emailVerified,
+          isAnonymous: user.isAnonymous,
+          uid: user.uid,
+        };
+
+        firestore
+          .collection('users')
+          .doc(user.uid)
+          .set(userDTO, { merge: true });
+
+        if (this.$route.name === 'login') {
+          this.$router.replace({ name: 'overview' });
+        }
+      }
+    });
+  },
+};
+</script>
 
 <style lang="postcss">
 @import '~@/app.pcss';
