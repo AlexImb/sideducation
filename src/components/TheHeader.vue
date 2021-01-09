bb96806c38225be4a7ca79b16217bc53d1b04b43
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 21:54:00 +0100

    Fix header centering

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,36 +1,42 @@
 <template>
   <header class="sticky top-0 bg-white shadow-md h-24">
-    <nav class="flex justify-between items-center p-4">
-      <div class="flex cursor-pointer w-24" @click="$router.go(-1)">
-        <template v-if="isAuth && !['overview', 'landing'].includes($route.name)">
-          <icon class="h-6 mx-1" :path="mdiChevronLeft" />
+    <nav class="flex items-center p-4">
+      <div class="flex-1">
+        <div
+          v-show="isAuth && !['overview', 'landing'].includes($route.name)"
+          class="flex cursor-pointer"
+          @click="$router.go(-1)"
+        >
+          <Icon class="h-6 mx-1" :path="mdiChevronLeft" />
           <div class="m-w-10 hidden md:block">
             Go back
           </div>
-        </template>
-      </div>
-      <router-link
-        class="font-lucky text-xl leading-1"
-        :to="{ name: isAuth ? 'overview' : 'landing' }"
-      >
-        <img src="/img/brand.png" class="h-16" />
-      </router-link>
-
-      <div v-if="isAuth" class="cursor-pointer flex" @click="logOut()">
-        <icon class="h-6 mx-1" :path="mdiLogoutVariant" />
-        <div class="m-w-10 hidden md:block">
-          Log out
         </div>
       </div>
 
-      <div
-        v-else
-        class="cursor-pointer flex font-semibold p-2"
-        @click="$router.push({ name: 'login' })"
-      >
-        <icon class="h-6 mx-1" :path="mdiLoginVariant" />
-        <div class="m-w-10 hidden md:block">
-          Log in
+      <div class="flex-auto flex justify-center">
+        <router-link :to="{ name: isAuth ? 'overview' : 'landing' }">
+          <img src="/img/brand.png" class="w-56" />
+        </router-link>
+      </div>
+
+      <div class="flex-1 flex justify-end">
+        <div v-if="isAuth" class="flex cursor-pointer" @click="logOut()">
+          <Icon class="h-6 mx-1" :path="mdiLogoutVariant" />
+          <div class="m-w-10 hidden md:block">
+            Log out
+          </div>
+        </div>
+
+        <div
+          v-else
+          class="cursor-pointer flex font-semibold p-2"
+          @click="$router.push({ name: 'login' })"
+        >
+          <Icon class="h-6 mx-1" :path="mdiLoginVariant" />
+          <div class="m-w-10 hidden md:block">
+            Log in
+          </div>
         </div>
       </div>
     </nav>
