b711825ad662636dd4d4a30bcf98f2aa1ed1739f
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-09 20:39:28 +0100

    Add Firebase analytics tracking

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,6 +7,8 @@ const config = JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG);
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
+firebase.analytics();
+
 export const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firebaseAuth = firebase.auth();
 export const firestore = firebase.firestore();
