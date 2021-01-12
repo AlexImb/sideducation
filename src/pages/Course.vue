d8483cb02fe2571388906c8dddbe238d955e4ec7
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2021-01-12 22:18:53 +0100

    Remove action button for teachers

--- src/pages/Course.vue
+++ src/pages/Course.vue
@@ -58,15 +58,14 @@
     <section class="my-4 text-gray-700">
       <div class="flex items-center justify-between">
         <h2 class="text-2xl font-semibold">Modules</h2>
-        <button v-if="isTeacher" class="btn">Edit questions</button>
         <router-link
-          v-else
+          v-if="!isTeacher"
           tag="button"
           class="btn"
           :to="{ name: 'learn', params: { id, qid: 1 } }"
         >
-          Resume learning</router-link
-        >
+          Resume learning
+        </router-link>
       </div>
       <article
         v-for="courseModule in visibleCourseModules"
